abd97541b92005facdcebf180eff8932ab52ad7e
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-12-29 10:00:10 +0100

    *

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,13 @@ function App() {
   const [isInView, setIsInView] = useState<string>('');
   const [lang, setLang] = useState<Lang>('PL')
   const [loginVisible, setLoginVisible] = useState<boolean>(false)
+  const [loggedin, setLoggedin] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>('');
   
   return (
     <div className="App">
-      <div className='control'><button onClick={()=> setLoginVisible(!loginVisible)} className='control--login'>{lang === 'PL' ? 'Zaloguj się' : "Login"}</button><Login lang={lang} loginVisible={loginVisible}/> <LangButton setLang={setLang} lang={lang}/></div>
+      <div className='login'>{loggedin ? <div>{lang === 'PL' ? 'Cześć': 'Hello'}, <span>{username}</span> ! <button onClick={()=> setLoggedin(false)} className='login__button'>{lang === 'PL' ? 'Wyloguj się' : "Logout"}</button></div>: <button onClick={()=> setLoginVisible(!loginVisible)} className='login__button'>{lang === 'PL' ? 'Zaloguj się' : "Login"}</button>}<Login setUsername={setUsername} setLoginVisible={setLoginVisible} setLoggedin={setLoggedin} lang={lang} loginVisible={loginVisible}/></div>
+      <div className='control'> <LangButton setLang={setLang} lang={lang}/></div>
       
        <SelectMenu lang={lang} setInView={setIsInView}/>
        <div className='App__container'>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,24 +2,67 @@
 "use client"
 import React, { useRef, useEffect, useState } from 'react'
 import './sass/Login.scss';
+import axios from 'axios';
 
 
-export const Login = ({lang, loginVisible}) => {
+export const Login = ({lang, loginVisible, setUsername, setLoggedin, setLoginVisible}) => {
     const [registerMode, setRegisterMode] = useState<boolean>(false);
+    const [login, setLogin] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      switch(registerMode){
+        case true:
+          axios.post('http://localhost:3000/users/new', { login, password, email })
+  .then((response) => {
+    console.log(response.data);
+    setEmail('');
+    setLogin('');
+    setPassword('');
+  })
+  .catch(error => {
+    console.error(error);
+  });
+          break;
+        case false:
+          axios.post('http://localhost:3000/users', { login, password })
+  .then((response) => {
+    console.log(response.data);
+    if(response.status === 200) {
+      setUsername(login);
+      setLogin('');
+      setPassword('');
+      setLoginVisible(false);
+      setLoggedin(true);
+    } else {
+
+    }
+    
+    
+  })
+  .catch(error => {
+    console.error(error);
+  });
+          break;
+      }
+    }
 
   return (
     
     <div className={`login-container ${loginVisible && 'visible'}`}>
         <div className='login-container__triangle'></div>
-        <form className='login-container__form'>
+        <form className='login-container__form' onSubmit={(e) => submit(e)}>
             <label htmlFor="login">Login:</label>
-            <input className='login-container__form--text' name='login' type="text" />
+            <input className='login-container__form--text' name='login' type="text" value={login} onChange={(e) => setLogin(e.target.value)} required/>
             <label htmlFor="password">{lang === 'PL' ? 'Hasło:' : 'Password:'}
             </label>
-            <input className='login-container__form--text' name='password' type="password" />
+            <input className='login-container__form--text' name='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
             {registerMode && <><label htmlFor="password">E-mail:
             </label>
-            <input className='login-container__form--text' name='email' type="email" /></>}
+            <input className='login-container__form--text' name='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/></>}
             <input className='login-container__form--submit' type="submit" value={lang === 'PL' ? registerMode === false ? 'Zaloguj się' : 'Zarejestruj się' : registerMode === true ? 'Register' : 'Login'} />
             <p className='login-container__form--p'>{lang === 'PL' ? <>{registerMode ? 'Masz już Konto ? ' : 'Nie masz jeszcze konta ? '}{<a onClick={()=> setRegisterMode(!registerMode)}>{registerMode ? lang === 'PL' ? 'Zaloguj się' : 'Login' : lang === 'PL' ? 'Zarejestruj się' : 'Register'}</a>}{registerMode ? ' 🏃🏻‍♂️' : ' aby śledzić swój progres ! 🏃🏻‍♂️'}</> : <>{registerMode ? 'Already have account ? ' : `Don't have account yet ? `}{<a onClick={()=> setRegisterMode(!registerMode)}>{registerMode ? 'Login' : 'Register'}</a>}{registerMode ? ' 🏃🏻‍♂️' : ` to track your progress !🏃🏻‍♂️`}</>}</p>
         </form>
